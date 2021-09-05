ff441c3d54d10213e5b346081940c082ddc8aeb0
Jeferson Furquim <jeferson.souza@cadis.com.br>
2021-09-05 01:54:23 -0300

    initial commit

---
+++ ListaTarefas/Models/TarefasContexto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListaTarefas.Models
+{
+    public class TarefasContexto : DbContext
+    {
+        public DbSet<Tarefas> Tarefas { get; set; }
+public TarefasContexto(DbContextOptions<TarefasContexto> opcoes) : base(opcoes)
+        {
+
+        }
+
+    }
+}
